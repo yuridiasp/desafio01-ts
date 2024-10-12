2b9100bc50d8455de76f35e47f25f2769c0a6061
Yuri Dias Pereira <yuristardias@hotmail.com>
2024-10-12 20:26:05 -0300

    Name e accountNumber somente leitura

--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -1,5 +1,5 @@
 export abstract class DioAccount {
-  private name: string
+  private readonly name: string
   private readonly accountNumber: number
   private balance: number = 0
   private status: boolean = true
@@ -7,11 +7,6 @@ export abstract class DioAccount {
   constructor(name: string, accountNumber: number){
     this.name = name
     this.accountNumber = accountNumber
-  }
-
-  setName = (name: string): void => {
-    this.name = name
-    console.log('Nome alterado com sucesso!')
   }
 
   getName = (): string => {
