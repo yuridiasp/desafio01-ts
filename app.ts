7ba2bfe75c68406db1e433f1971a57fdf414fd88
Yuri Dias Pereira <yuristardias@hotmail.com>
2024-10-12 20:31:52 -0300

    Criado instancias e executado os métodos no arquivo principal

--- app.ts
+++ app.ts
@@ -1,9 +1,25 @@
 import { CompanyAccount } from './class/CompanyAccount'
 import { PeopleAccount } from './class/PeopleAccount'
+import { VipAccount } from './class/VipAccount'
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
+const vipAccount = new VipAccount('Poliana', 30)
+const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Yuri', 10)
 const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+
+vipAccount.deposit(50)
+vipAccount.getBalance()
+vipAccount.getName()
+vipAccount.validateStatus()
+vipAccount.withdraw(10)
+
+peopleAccount.deposit(40)
+peopleAccount.getBalance()
+peopleAccount.getName()
+peopleAccount.validateStatus()
+peopleAccount.withdraw(20)
+
+companyAccount.deposit(51)
+companyAccount.getBalance()
+companyAccount.getLoan(60)
+companyAccount.getName()
+companyAccount.withdraw(15)
